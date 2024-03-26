04d8e4b2002dd2aee23c5aa3922fef8c9a93ffb7
santibrizuela <santibrizuela.97@gmail.com>
2024-03-26 18:48:22 -0300

    202403261848

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "@/store/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({
   Component,
@@ -13,13 +14,13 @@ export default function App({
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        
+        <SessionProvider session={session}>
           <div className="font-bodyFont bg-gray-300">
             <RootLayout>
               <Component {...pageProps} />
             </RootLayout>
           </div>
-        
+        </SessionProvider>
       </PersistGate>
     </Provider>
   );
